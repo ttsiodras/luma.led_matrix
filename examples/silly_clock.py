216ad081d4ed87f70e24d70b88eb41b260a7a3f7
Thanassis Tsiodras <ttsiodras@gmail.com>
2019-04-21 10:15:29 +0200

    Way too bright during the night - this contrast setting works fine.

--- examples/silly_clock.py
+++ examples/silly_clock.py
@@ -47,7 +47,7 @@ def main():
     # Setup for Banggood version of 4 x 8x8 LED Matrix (https://bit.ly/2Gywazb)
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, cascaded=4, block_orientation=-90, blocks_arranged_in_reverse_order=True)
-    device.contrast(16)
+    device.contrast(2)
 
     # The time ascends from the abyss...
     animation(device, 8, 1)
